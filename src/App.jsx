7507c7a26591bf9cc92ea7dc44acbbb067e5e80f
Ahmed Yasser <ahmdyassr@gmail.com>
2022-10-31 03:31:47 +0200

    Add app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,28 @@
-export default function App() {
+import './styles/global.css'
+import { useState } from 'react'
+import Container from './components/Container'
+import Alert from './components/Alert'
+import Header from './components/Header'
+import Form from './components/Form'
+import List from './components/List'
+import Result from './components/Result'
+import expensesData from './data/expenses'
+
+const App = () => {
+	const [expenses, setExpenses] = useState(expensesData)
+	const total = expenses.reduce( (acc, curr) => {
+		return acc += curr.amount
+	}, 0)
+
 	return (
-		<h3>Hello</h3>
+		<Container>
+			<Alert />
+			<Header />
+			<Form />
+			<List expenses={expenses} />
+			<Result total={total} />
+		</Container>
 	)
 }
+
+export default App
