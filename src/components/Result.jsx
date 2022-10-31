cea84da6c6e0fc0e311e0a022eb60e9afbacae93
Ahmed Yasser <ahmdyassr@gmail.com>
2022-10-31 12:08:55 +0200

    Improve the result component styling

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,6 +1,20 @@
-const Result = ({ total }) => {
+import styles from '../styles/Result.module.css'
+import dollarize from '../utils/dollarize'
+
+const Result = ({ expenses }) => {
+	const total = expenses?.reduce( (acc, curr) => {
+		return acc += parseInt(curr.amount)
+	}, 0)
+
 	return (
-		<p>Total Spendings is: {total || 0}</p>
+		<>
+			{expenses.length > 0 && (
+				<div className={styles.result}>
+					<p className={styles.result__title}>Total</p>
+					<p className={styles.result__total}>{dollarize(total)}</p>
+				</div>
+			)}
+		</>
 	)
 }
 
