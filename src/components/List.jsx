4c956e8d2a598029530332d32f23f92a34179442
Ahmed Yasser <ahmdyassr@gmail.com>
2022-10-31 12:09:27 +0200

    Improve list component styling

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,13 +1,24 @@
 import styles from '../styles/List.module.css'
 import Item from './Item'
 
-const List = ({ expenses }) => {
+const List = ({ expenses, updateExpenses, clearExpenses}) => {
 	return (
 		<div className={styles.list}>
+			<div className={styles.list__header}>
+				<h3 className={styles.list__headerTitle}>Items</h3>
+
+				{expenses.length > 0 && (
+					<button className={styles.list__headerButton} onClick={clearExpenses}>Clear All</button>
+				)}
+			</div>
 			{
-				expenses?.map( ({id, title, amount}) => (
-					<Item key={id} title={title} amount={amount} />
-				))
+				expenses.length ? (
+					expenses.map( ({id, title, amount}) => (
+						<Item key={id} id={id} title={title} amount={amount} handleDelete={updateExpenses} />
+					))
+				) : (
+					<p className={styles.list__emptyText}>✎ No items in the list, Start adding ones!</p>
+				)
 			}
 		</div>
 	)
