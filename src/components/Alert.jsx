c73b14411b2de145c0dea5dbcddbe6ccc859ed91
Ahmed Yasser <ahmdyassr@gmail.com>
2022-10-31 03:29:43 +0200

    Add alert component

---
+++ src/components/Alert.jsx
@@ -0,0 +1,11 @@
+import styles from '../styles/Alert.module.css'
+
+const Alert = () => {
+	return (
+		<div className={styles.alert}>
+			<p className={styles.alert__title}>Alert</p>
+		</div>
+	)
+}
+
+export default Alert
