ebc521340ac5f3d337ca2c9f58bdf6cdf5d475f5
Ahmed Yasser <ahmdyassr@gmail.com>
2022-10-31 03:31:15 +0200

    Add container component

---
+++ src/components/Container.jsx
@@ -0,0 +1,11 @@
+import styles from '../styles/Container.module.css'
+
+const Container = ({ children }) => {
+	return (
+		<div className={styles.container}>
+			{ children }
+		</div>
+	)
+}
+
+export default Container
