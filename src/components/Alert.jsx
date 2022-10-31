df45d5c1685a137005463c77e9a6f7bcb60ebc73
Ahmed Yasser <ahmdyassr@gmail.com>
2022-10-31 12:08:24 +0200

    Add show and hide functionality to the alert

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,9 +1,9 @@
 import styles from '../styles/Alert.module.css'
 
-const Alert = () => {
+const Alert = ({isShown, message}) => {
 	return (
-		<div className={styles.alert}>
-			<p className={styles.alert__title}>Alert</p>
+		<div className={`${styles.alert} ${ isShown ? styles.isShown : ''}`}>
+			<p className={styles.alert__title}>{message}</p>
 		</div>
 	)
 }
