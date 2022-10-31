c22354f6451cdc1d51c5a7a205bf36a24ea1f956
Ahmed Yasser <ahmdyassr@gmail.com>
2022-10-31 03:30:56 +0200

    Add temporary sample data

---
+++ src/data/expenses.js
@@ -0,0 +1,17 @@
+export default [
+	{
+		id: 1,
+		title: 'Rent',
+		amount: 1600
+	},
+	{
+		id: 2,
+		title: 'Car Payment',
+		amount: 600
+	},
+	{
+		id: 3,
+		title: 'Credit Card Bill',
+		amount: 3400
+	}
+]
